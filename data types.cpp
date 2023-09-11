6b1c9f3f3439921ed2ac3f499020c80281894a8e
umayhabiba <142391346+umayhabiba@users.noreply.github.com>
2023-09-11 21:04:12 +0500

    Add files via upload

---
+++ data types.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+//data types
+	string name ="umay habiba";
+	int age=19;
+	char section='a';
+	string subject="programming";
+	int marks=98;
+	float avg=7.90;
+	cout << "students details \n";
+	cout <<"name="<<name<<"\n";
+	cout <<"age="<<age <<"\n";
+	cout <<"section="<<section<<"\n";
+	cout <<"subject="<<subject<<"\n";
+	cout <<"marks="<<marks<<"\n";
+	return 0;
+}
+	
