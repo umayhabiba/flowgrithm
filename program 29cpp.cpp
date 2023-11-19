98adf220a3292a5dd689df70e8a65b8f9521490d
umayhabiba <142391346+umayhabiba@users.noreply.github.com>
2023-11-19 15:17:00 +0500

    Create program 29cpp.cpp

---
+++ program 29cpp.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,c=1;
+    cout<<"enter an number for table""\n";
+    cin>>n;
+    while(c<=10){
+                 cout<<n<<"*"<<c<<"="<<n*c<< "\n";
+                 c++;
+                 }
+                 return 0;
+}
