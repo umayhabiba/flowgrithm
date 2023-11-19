2911da8e194ea0f081f5be908df9e8171d4a3967
umayhabiba <142391346+umayhabiba@users.noreply.github.com>
2023-11-19 14:55:48 +0500

    Create program 3cpp.cpp

---
+++ program 3cpp.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int b,h,area;
+	cout<<" enter the base:";
+	cin>> b;
+	cout<<"enter the height:";
+	cin>>h;
+	area=0.4*b*h;
+	cout<<"area="<<area;
+	return 0;
+}
