057a34d11f0c2e8ebb1df834e0cf2b8e64b29767
umayhabiba <142391346+umayhabiba@users.noreply.github.com>
2023-11-19 15:08:18 +0500

    Create program 20cpp.cpp

---
+++ program 20cpp.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int temp;
+    cout<<"enter the temp""\n";
+    cin>>temp;
+    if(temp>35){
+                cout<<"hot day" "\n";
+}
+    else if(temp>=25){
+         cout<<"pleasant day" "\n";
+}
+    else if(temp<25){
+         cout<<"cool day" "\n";
+}
+   return 0;
+}
