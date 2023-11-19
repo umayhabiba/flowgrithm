7561b1e39d3f7abdda3a8d01ed0ceebb698165c2
umayhabiba <142391346+umayhabiba@users.noreply.github.com>
2023-11-19 15:32:03 +0500

    Create program 51.cpp

---
+++ program 51.cpp
@@ -0,0 +1,18 @@
+//Title:: Write a program that displays the following block using nested for loop.
+//*****
+//*****
+//*****
+//*****
+//*****
+#include<iostream>
+using namespace std;
+int main ()
+{
+	int i,j;
+	for(int i=1; i<=5; i++){
+		for(int j=1; j<=5; j++){
+		cout<<"*";
+	}
+	cout<<endl;
+	}
+}
