539c29807074311b9c6b1442c29cc1849bf230ed
umayhabiba <142391346+umayhabiba@users.noreply.github.com>
2023-11-19 15:26:23 +0500

    Create program 42cpp.cpp

---
+++ program 42cpp.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float r;
+    r=1;
+    for(int c=1;c<=50;c=c+2){
+              r=r+1.0/c;
+              }
+              cout<<"result is" <<r<< "\n";
+              return 0;
+    }
