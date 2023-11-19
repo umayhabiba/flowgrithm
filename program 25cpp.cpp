b1adc83306be0c25292b72a77afef3ac3234914f
umayhabiba <142391346+umayhabiba@users.noreply.github.com>
2023-11-19 15:14:01 +0500

    Create program 25cpp.cpp

---
+++ program 25cpp.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n=1;
+    while(n<=10){
+                 cout<<n<< "\n";
+                 n++;
+                 }
+      return 0;
+}
