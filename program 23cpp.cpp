5bcfdb2e2d52dd541822e73c3c04a1c8a5deed24
umayhabiba <142391346+umayhabiba@users.noreply.github.com>
2023-11-19 15:12:09 +0500

    Create program 23cpp.cpp

---
+++ program 23cpp.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char ch;
+    cout<<"Enter a character";
+    cin>>ch;
+    switch(ch){
+              case 'a':
+                   cout<<"ch is vowel" "\n";
+                   break;
+              case 'e':
+                   cout<<"ch is vowel" "\n";
+                   break;
+              case 'i':
+                   cout<<"ch is vowel" "\n";
+                   break;
+              case 'o':
+                   cout<<"ch is vowel" "\n";
+                   break;
+              case 'u':
+                   cout<<"ch is vowel" "\n";
+                   break;
+              default:
+                      cout<<"ch is consonant" "\n";
+              }
+              return 0;
