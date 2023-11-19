799b17dec592e2f0ea4815b27c36ae59c8d165ea
umayhabiba <142391346+umayhabiba@users.noreply.github.com>
2023-11-19 14:56:28 +0500

    Create program 4 cpp.cpp

---
+++ program 4 cpp.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  float C,F;
+  cout<<"enter the value of temp in cel:";
+  cin>>C;
+  F=9/5*C+32;
+  cout<<"temp in frh"<<F;
+  return 0;
+}
