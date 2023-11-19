844e8a3c298d5b924b2c100c87f518de4516fb31
umayhabiba <142391346+umayhabiba@users.noreply.github.com>
2023-11-19 14:51:37 +0500

    datatypes.cpp

---
+++ datatypes.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  // we can find the size of any datatype in cpp using sizeof() operators
+  cout << "the size of int is" << sizeof(int) << "bytes" << endl;
+  cout << "the size of int is" << sizeof(bool) << "bytes" << endl;
+  cout << "the size of int is" << sizeof(float) << "bytes" << endl;
+  cout << "the size of int is" << sizeof(double) << "bytes" << endl;
+  cout << "the size of int is" << sizeof(char) << "bytes" << endl;
+  return 0;
+}
