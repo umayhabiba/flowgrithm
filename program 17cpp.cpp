32a45c0e62eb2a635962f466a9a7090f0bac8008
umayhabiba <142391346+umayhabiba@users.noreply.github.com>
2023-11-19 15:05:38 +0500

    Create program 17cpp.cpp

---
+++ program 17cpp.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout<<"enter a num""\n";
+    cin>>num;
+    if(num>0){
+           cout<<"enter num is positive""\n";
+}
+    if (num<0){
+              cout<<"enter num is negative""\n";
+}
+    if(num==0){
+            cout<<"num is zero""\n";
+}
+    return 0;
+}
