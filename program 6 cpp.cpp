e6936b226780b3787f20a0b7666578f678d3fc14
umayhabiba <142391346+umayhabiba@users.noreply.github.com>
2023-11-19 14:57:56 +0500

    Create program 6 cpp.cpp

---
+++ program 6 cpp.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int h;
+	float h_in_c;
+	cout<<"enter the h:";
+	cin>>h;
+	h_in_c=h*2.54;
+	cout<<"your h in c is:"<<h_in_c;
+	return 0;
+	
+}
