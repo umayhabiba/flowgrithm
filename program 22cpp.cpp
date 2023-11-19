ead72a9ea362da32cf59c4d8237b03fb79d8e1c6
umayhabiba <142391346+umayhabiba@users.noreply.github.com>
2023-11-19 15:11:25 +0500

    Create program 22cpp.cpp

---
+++ program 22cpp.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout<<"enter 3 numbers" "\n";
+    cin>>a>>b>>c;
+    if(a>b&&a>c){
+                 cout<<"a is maximum number"<<a<< "\n";
+                 }
+                 else if(b>c&&b>a){
+                 cout<<"b is maximum number"<<b<< "\n";
+                 }
+                 else
+                      cout<<"c is maximum number"<<c<< "\n";
+          return 0;
+          }
