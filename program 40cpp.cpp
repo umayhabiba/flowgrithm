749f1fe179c1e7268cca58c58156a45ffdce8cb1
umayhabiba <142391346+umayhabiba@users.noreply.github.com>
2023-11-19 15:25:01 +0500

    Create program 40cpp.cpp

---
+++ program 40cpp.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter the num";
+    cin>>n;
+    for(int i=1;i<=10;i++){
+            cout<<n<< "*" <<i<< "=" <<n<< "\n";
+            }
+            return 0;
+   }
