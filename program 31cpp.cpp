8faac5863fb8ed072cf2e3fd5db7c30438640e7e
umayhabiba <142391346+umayhabiba@users.noreply.github.com>
2023-11-19 15:19:15 +0500

    Create program 31cpp.cpp

---
+++ program 31cpp.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float c,r;
+    c=2;
+    r=1;
+    while(c<=100){
+                  r=r+1.0/c;
+                  c=c+2;
+                  }
+                  cout<<"result"<<r<< "\n";
+                  return 0;
+}
