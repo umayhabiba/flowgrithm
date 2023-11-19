9dcd2915d023d3772afdf77a190a97232316b93e
umayhabiba <142391346+umayhabiba@users.noreply.github.com>
2023-11-19 15:20:01 +0500

    Create program 33cpp.cpp

---
+++ program 33cpp.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int s,e,n;
+    cout<<"enter starting num" "\n";
+    cin>>s;
+    cout<<"enter ending num" "\n";
+    cin>>e;
+    do{
+           if(n%2!=0)
+           cout<<n<< "\n";
+           n++;
+           }
+           while(n<=e);
+           return 0;
+ }
