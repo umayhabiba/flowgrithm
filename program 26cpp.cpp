771af93faafd749f3bb1e556c8b37622f11f09a5
umayhabiba <142391346+umayhabiba@users.noreply.github.com>
2023-11-19 15:14:54 +0500

    Create program 26cpp.cpp

---
+++ program 26cpp.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n=1,sum=0;
+    while(n<=5){
+                cout<<n<< "\n";
+                sum=sum+n;
+                n++;
+                }
+                cout<<"sum ="<<sum<< "\n";
+             return 0;
+}
