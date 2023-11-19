18f438febb8d4bac9841c0bf6d9bb0dd03428e57
umayhabiba <142391346+umayhabiba@users.noreply.github.com>
2023-11-19 15:01:45 +0500

    Create program 13cpp.cpp

---
+++ program 13cpp.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int cel,frh;
+    cout<<"enter temp in frh";
+    cin>>frh;
+    cel=(frh-32)*5/9;
+    cout<<"temp in cel"<<cel<<"\n";
+    return 0;
+}
