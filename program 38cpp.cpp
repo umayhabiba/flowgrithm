a31675a022903814704edc34499cdda42da151e3
umayhabiba <142391346+umayhabiba@users.noreply.github.com>
2023-11-19 15:23:36 +0500

    Create program 38cpp.cpp

---
+++ program 38cpp.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int p=1;
+    for(int i=1;i<=10;i=i+2){
+             p=p*i;
+             }
+             cout<<"result="<<p<< "\n";
+             return 0;
+}
