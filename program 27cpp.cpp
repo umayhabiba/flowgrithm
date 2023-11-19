087940c0d4e078bdacfdb9a6da291d51fea9219c
umayhabiba <142391346+umayhabiba@users.noreply.github.com>
2023-11-19 15:15:38 +0500

    Create program 27cpp.cpp

---
+++ program 27cpp.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n=1;
+    while(n<=5){
+                cout<<n<<"   "<<n*n<< "\n";
+                n++;
+                }
+                return 0;
+}
