7abcb8137654a3ec2b5cf2ad1a9c9d71a616bc26
umayhabiba <142391346+umayhabiba@users.noreply.github.com>
2023-11-19 15:17:51 +0500

    Create program 2cpp.cpp

---
+++ program 2cpp.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int age;
+	string address;
+	char name;
+	cout<< "enter the name";
+	cin>> name;
+	cout<<"enter the age";
+	cin>> age;
+	cout<<"enter the address";
+	cin>> address;
+    cout<<"your name is="<<name<<"\n";
+    cout<<"your age is="<<age<<"\n";
+    cout<<"your address is="<<address<<"\n";
+    return 0;
+}
